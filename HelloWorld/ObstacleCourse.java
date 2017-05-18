9fe55737364db86184a839aecf7fc5f97bd74cea
saanvid <SDhingra@student.naperville203.org>
2017-05-18 14:11:41 -0500

    sas

--- HelloWorld/ObstacleCourse.java
+++ HelloWorld/ObstacleCourse.java
@@ -10,48 +10,55 @@ import lejos.robotics.navigation.DifferentialPilot;
 public class ObstacleCourse implements SensorPortListener, FeatureListener
 {
     DifferentialPilot pilot ;
-    
+
     public ObstacleCourse ()
     {
         pilot = new DifferentialPilot(2.25f, 5.5f, Motor.B, Motor.C);
         pilot.forward();
     }
-    
+
     public static void main(String[] args)
     {
         ObstacleCourse c = new ObstacleCourse();
-        
-        
+        TouchSensor touch = new TouchSensor(SensorPort.S3);
+       // while (touch.isPressed())
+       // {
+       //     touchSensor();
+        //}
         SensorPort.S2.addSensorPortListener(c);
-         TouchSensor touch = new TouchSensor(SensorPort.S3);
         
-		UltrasonicSensor us = new UltrasonicSensor(SensorPort.S1);
-         RangeFeatureDetector fd = new RangeFeatureDetector(us, 80, 500);
-         
-         fd.addListener(c);
-         
-         
-         
+        
+        UltrasonicSensor us = new UltrasonicSensor(SensorPort.S1);
+        RangeFeatureDetector fd = new RangeFeatureDetector(us, 80, 500);
+
+        fd.addListener(c);
+
+        Button.waitForAnyPress();
+
     }
+
    
-    
-        public void stateChanged(SensorPort aSource,int aOldValue,int aNewValue)  
+
+    public void stateChanged(SensorPort aSource,int aOldValue,int aNewValue)  
     {
         LightSensor light = new LightSensor(SensorPort.S2);
         LCD.drawInt(light.getLightValue(), 4, 0, 0);
         LCD.drawInt(light.getNormalizedLightValue(), 4, 0, 1);
         LCD.drawInt(SensorPort.S2.readValue(), 4, 0, 3);
-      
+
         if( light.getLightValue() < 35)
         {
-            
-
+            pilot.rotate(90);
+            pilot.forward();
         }
     }
-    
+
     public void featureDetected(Feature feature, FeatureDetector detector) {
+        pilot.stop();
         int range = (int)feature.getRangeReading().getRange();
-        Sound.playTone(1200 - (range * 10), 100);
-        System.out.println("Range:" + range);
+        int angle = (int)feature.getRangeReading().getAngle();
+        pilot.rotate(angle);
+        pilot.travel(range);
+
     }
 }
