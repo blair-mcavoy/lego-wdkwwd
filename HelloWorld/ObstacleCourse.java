ff86295906c1c0e9c159733ee3b85250d19ed02c
saanvid <SDhingra@student.naperville203.org>
2017-05-25 09:23:57 -0500

    zsd

--- HelloWorld/ObstacleCourse.java
+++ HelloWorld/ObstacleCourse.java
@@ -11,21 +11,26 @@ import lejos.robotics.navigation.DifferentialPilot;
 public class ObstacleCourse implements SensorPortListener, FeatureListener
 {
     DifferentialPilot pilot ;
-
+    private boolean isPressed;
+    private int count;
+    private int angle;
     public ObstacleCourse ()
     {
+        angle = 45;
+        count = 1;
+        isPressed = false;
         pilot = new DifferentialPilot(2.25f, 5.5f, Motor.B, Motor.C);
         pilot.forward();
     }
 
     public static void main(String[] args)
     {
+
         ObstacleCourse c = new ObstacleCourse();
-        
-   
+
         SensorPort.S2.addSensorPortListener(c);
         SensorPort.S3.addSensorPortListener(c);
-            
+
         UltrasonicSensor us = new UltrasonicSensor(SensorPort.S1);
         RangeFeatureDetector fd = new RangeFeatureDetector(us, 80, 500);
 
@@ -35,41 +40,57 @@ public class ObstacleCourse implements SensorPortListener, FeatureListener
 
     }
 
-   
-
     public void stateChanged(SensorPort aSource,int aOldValue,int aNewValue)  
     {
         if (aSource == SensorPort.S2)
         {
-        LightSensor light = new LightSensor(SensorPort.S2);
-        LCD.drawInt(light.getLightValue(), 4, 0, 0);
-        LCD.drawInt(light.getNormalizedLightValue(), 4, 0, 1);
-        LCD.drawInt(SensorPort.S2.readValue(), 4, 0, 3);
+            LightSensor light = new LightSensor(SensorPort.S2);
+            LCD.drawInt(light.getLightValue(), 4, 0, 0);
+            LCD.drawInt(light.getNormalizedLightValue(), 4, 0, 1);
+            LCD.drawInt(SensorPort.S2.readValue(), 4, 0, 3);
 
-        if( light.getLightValue() < 35)
-        {
-            pilot.rotate(90);
-            pilot.forward();
-        }
-      }
-       else if (aSource == SensorPort.S3)
-       {   TouchSensor touch = new TouchSensor(SensorPort.S3);
-           if (touch.isPressed())
-           {
-             pilot.travel(-10);
-             pilot.rotate(90);
-             pilot.forward(); 
+            if( light.getLightValue() < 35)
+            {
+                pilot.travel(-10);
+                pilot.rotate(angle);
+              
+                pilot.forward();
+              
+                if(angle<315)
+                {
+                    angle += 45;
+                }
+                else
+                {
+                    angle = 45;
+                }
             
-           } 
-       }
+            }
+        }
+        else if (aSource == SensorPort.S3)
+        {   TouchSensor touch = new TouchSensor(SensorPort.S3);
+            if (touch.isPressed())
+            {
+                isPressed = true;
+                pilot.travel(-5);
+                pilot.rotate(90);
+                pilot.forward(); 
+                
+            } 
+                isPressed = false;
+        }
     }
 
     public void featureDetected(Feature feature, FeatureDetector detector) {
-        pilot.stop();
-        int range = (int)feature.getRangeReading().getRange();
-        int angle = (int)feature.getRangeReading().getAngle();
-        pilot.rotate(angle);
-        pilot.travel(range);
-
+        if (isPressed == false)
+        {
+            pilot.stop();
+           
+            int range = (int)feature.getRangeReading().getRange();
+            int angle = (int)feature.getRangeReading().getAngle();
+             LCD.drawInt(range,0,5);
+            pilot.rotate(angle);
+            pilot.travel(range);
+        }
     }
 }
